ec4eaad834e906fb417bcf474e191b8b25fb4f4a
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-04-09 18:44:26 -0300

    add apps and urls

--- healthbuddy_backend/settings.py
+++ healthbuddy_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "set your secret key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': os.environ.get("DB_NAME", "healthbuddy"),
         'USER': os.environ.get("DB_USER", "ilha_healthbuddy"),
         'PASSWORD': os.environ.get("DB_PASSWORD", "ilha102030"),
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
@@ -130,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- healthbuddy_backend/urls.py
+++ healthbuddy_backend/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include("dashboard.urls")),
-    path("users/", include("users.urls")),
-    path("publications/", include("publications.urls")),
-    path("videos/", include("videos.urls")),
-    path("api/", include("api.urls")),
+    path('dashboard/', include("healthbuddy_backend.dashboard.urls")),
+    path("users/", include("healthbuddy_backend.users.urls")),
+    path("publications/", include("healthbuddy_backend.publications.urls")),
+    path("videos/", include("healthbuddy_backend.videos.urls")),
+    path("api/", include("healthbuddy_backend.api.urls")),
 ]
