f78316a4eb074e07a83468049919772e79470479
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-04-14 17:51:07 -0300

    add template login and view login

--- healthbuddy_backend/settings.py
+++ healthbuddy_backend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from django.urls import reverse_lazy
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,6 +43,10 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
+    "widget_tweaks"
+]
+
+INSTALLED_APPS += [
     "healthbuddy_backend.dashboard",
     "healthbuddy_backend.users",
     "healthbuddy_backend.publications",
@@ -62,7 +69,7 @@ ROOT_URLCONF = 'healthbuddy_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,4 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#LOGIN_REDIRECT_URL = reverse_lazy("list_user")
+LOGOUT_REDIRECT_URL = reverse_lazy("login")
+LOGIN_URL = reverse_lazy("login")
--- healthbuddy_backend/urls.py
+++ healthbuddy_backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,6 @@ urlpatterns = [
     path("publications/", include("healthbuddy_backend.publications.urls")),
     path("videos/", include("healthbuddy_backend.videos.urls")),
     path("api/", include("healthbuddy_backend.api.urls")),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
